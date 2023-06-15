8a53d5beb896d476757f9c3ce16211842d2e58b0
Abn Alsrag <85142438+AbnAlsrag@users.noreply.github.com>
2023-06-15 10:21:40 +0300

    Removed label

--- src/DECI.py
+++ src/DECI.py
@@ -1,39 +1,11 @@
-"""
-    ██████╗ ███████╗ ██████╗██╗    ██╗  ██╗███████╗██╗     ██╗     
-    ██╔══██╗██╔════╝██╔════╝██║    ██║  ██║██╔════╝██║     ██║     
-    ██║  ██║█████╗  ██║     ██║    ███████║█████╗  ██║     ██║     
-    ██║  ██║██╔══╝  ██║     ██║    ██╔══██║██╔══╝  ██║     ██║     
-    ██████╔╝███████╗╚██████╗██║    ██║  ██║███████╗███████╗███████╗
-    ╚═════╝ ╚══════╝ ╚═════╝╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝    
-
-    This Software Was Made by ABN ALSRAG
-    DECI HELL
-    
-    This software is provided 'as-is', without any express or implied
-    warranty.  In no event will the authors be held liable for any damages
-    arising from the use of this software.
-
-    Permission is granted to anyone to use this software for any purpose,
-    including commercial applications, and to alter it and redistribute it
-    freely, subject to the following restrictions:
-
-    1. The origin of this software must not be misrepresented; you must not
-        claim that you wrote the original software. If you use this software
-        in a product, an acknowledgment in the product documentation would be
-        appreciated but is not required.
-    2. Altered source versions must be plainly marked as such, and must not be
-        misrepresented as being the original software.
-    3. This notice may not be removed or altered from any source distribution.
-"""
-
 # GUI IS AN DEPRECATED IDIA DON'T BOTHER LOOKING AT IT
 import os
 import time
 import random
 
 
+# Containre class for possible options
 class Scenario:
-    # Containre class for possible options
     txt: str = ""
 
     def __init__(self, txt: str) -> None:
@@ -150,7 +122,7 @@ class Game:
             )
             self.__slow_print(
                 f"Invalid input {input_string} "
-                + "is not a  valid input the only valid input is integer "
+                + "is not a valid input the only valid input is integer "
                 + "numbers (0-9)"
             )
             self.__slow_print(
